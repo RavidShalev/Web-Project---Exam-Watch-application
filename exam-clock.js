a86451d5a0ed6e96c3f91175a5a162054f166d78
amitderei <amitderei123@gmail.com>
2025-11-26 22:47:25 +0200

    changed: exam-clock.html, exam-clock.js
    -add live clock
    -create a draft of a log event (in the exam)

--- exam-clock.js
+++ exam-clock.js
@@ -3,6 +3,7 @@ const EXAM_DURATION_MINUTES = 180; // 3 hours
 
 let remainingSeconds = EXAM_DURATION_MINUTES * 60;
 let intervalId = null;
+let flag=false;
 
     function formatTime(totalSeconds) {
             const hours   = Math.floor(totalSeconds / 3600);
@@ -21,6 +22,7 @@ let intervalId = null;
 
             const countdownEl = document.getElementById("countdown");
             countdownEl.textContent = formatTime(remainingSeconds);
+            const addEventBtn = document.getElementById("addEventBtn");
 
             intervalId = setInterval(() => {
                 remainingSeconds--;
@@ -29,11 +31,39 @@ let intervalId = null;
                     clearInterval(intervalId);
                     intervalId = null;
                     countdownEl.textContent = "Exam Finished";
+                    disableStartButton();
                     return;
                 }
 
                 countdownEl.textContent = formatTime(remainingSeconds);
             }, 1000);
+            addEventBtn.disabled =false;
+            addEventBtn.classList.remove("bg-gray-400", "cursor-not-allowed");
+            addEventBtn.classList.add("bg-blue-500", "hover:bg-blue-600");
+            
     }
+
+    function updateClock() {
+            if (flag===false){
+                disableStartButton();
+                flag=true;
+            }
+            const now = new Date();
+            let hours = String(now.getHours()).padStart(2, '0');
+            let minutes = String(now.getMinutes()).padStart(2, '0');
+            let seconds = String(now.getSeconds()).padStart(2, '0');
+
+            document.getElementById("liveClock").innerText =`${hours}:${minutes}:${seconds}`;
+            
+    }
+    
+    function disableStartButton(){
+        const addEventBtn = document.getElementById("addEventBtn");
+        addEventBtn.disabled = true;
+        addEventBtn.classList.add("bg-gray-400", "cursor-not-allowed");
+        addEventBtn.classList.remove("bg-blue-500", "hover:bg-blue-600");
+    }
+setInterval(updateClock, 1000);
+updateClock();
 
 window.startCountdown = startCountdown;
