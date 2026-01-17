e43235a51f2fd12a3838a784eb4aba026acbdd47
amitderei <amitderei123@gmail.com>
2026-01-17 20:01:16 +0200

    add adjustment to the phone
    of transfer student

--- exam_monitoring/app/(main)/active-exam/[examId]/attendanceList.tsx
+++ exam_monitoring/app/(main)/active-exam/[examId]/attendanceList.tsx
@@ -259,6 +259,15 @@ export default function AttemdanceList({
               </button>
 
               <button
+                disabled={record.attendanceStatus === "finished" || record.attendanceStatus === "transferred"}
+                onClick={() => {setSelectedRecord(record); setOpenTransfer(true);}}
+                className="flex-1 rounded-xl px-3 py-2 text-xs font-semibold text-white bg-[var(--purple)] disabled:bg-[var(--border)]
+                  disabled:text-[var(--muted)]">
+                העבר כיתה
+              </button>
+
+
+              <button
                 disabled={record.attendanceStatus === "absent" || record.attendanceStatus ==="finished"}
                 onClick={() => {
                   setSelectedRecord(record);
